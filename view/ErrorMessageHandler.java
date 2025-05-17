6d7da39c058604a54ca0de535256d91c9bd25649
Andreas <andreas.a.98@hotmail.com>
2025-05-17 20:58:14 +0200

    Add files via upload

---
+++ view/ErrorMessageHandler.java
@@ -0,0 +1,13 @@
+package se.kth.iv1350.view;
+
+public class ErrorMessageHandler {
+
+    void showErrorMessage (String message){
+        StringBuilder errorInfo = new StringBuilder();
+        errorInfo.append("Error: ");
+
+        errorInfo.append(message);
+        System.out.println(errorInfo);
+    }
+
+}
