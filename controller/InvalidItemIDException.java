96bad1c32880536da98bdc13fdcd06245fba92dc
Andreas <andreas.a.98@hotmail.com>
2025-05-17 20:59:56 +0200

    Update InvalidItemIDException.java

--- controller/InvalidItemIDException.java
+++ controller/InvalidItemIDException.java
@@ -1,6 +1,7 @@
 package se.kth.iv1350.controller;
 
 public class InvalidItemIDException extends Exception{
+    
     public InvalidItemIDException (String message){
         super(message);
     }
