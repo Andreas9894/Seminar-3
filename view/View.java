284bc51cc263144e9810bd8742f41af88cb2e120
Andreas <andreas.a.98@hotmail.com>
2025-05-19 19:14:55 +0200

    Add files via upload

--- view/View.java
+++ view/View.java
@@ -2,16 +2,20 @@ package se.kth.iv1350.view;
 
 import java.util.LinkedList;
 
+import se.kth.iv1350.controller.ConnectionException;
 import se.kth.iv1350.controller.Controller;
+import se.kth.iv1350.controller.ItemIDException;
 
 /**
  * This is a placeholder for the actual view. it contains calls to all system operations in the controller 
  */
 public class View {
     private Controller contr;
+    private ErrorMessageHandler errorMessageHandler;
 
     public View (Controller contr){
         this.contr = contr;
+        this.errorMessageHandler = new ErrorMessageHandler();
     }
 
     public void trialExecution () {
@@ -19,8 +23,11 @@ public class View {
         LinkedList <String> items = new LinkedList<String>();
         items.add("choklad231");
         items.add("brod332");
+        items.add("laptop192");
         items.add("choklad231");
         items.add("lask256");
+        items.add("fail");
+        
 
         contr.startSale();
         TotalRevenueView revenueView = new TotalRevenueView();
@@ -30,7 +37,17 @@ public class View {
         contr.addRevenueObserver(revenueFile);
 
          for (String item : items){
+            try{
             contr.scanItem(item);
+            }
+            catch (ItemIDException e){
+                errorMessageHandler.showErrorMessage(e.getMessage());
+                continue;
+            } catch (Exception e){
+                errorMessageHandler.showErrorMessage(e.getMessage());
+                System.exit(1);
+
+            }
             }    
     
         contr.endSale();
